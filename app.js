da3087708bd5f0018be15168b07b25257dc8e1e8
gam-c <stavish@gmail.com>
2022-06-03 10:47:05 -0400

    add

--- app.js
+++ app.js
@@ -15,11 +15,18 @@ console.log(arrLength([1, 2, 3]))
 console.log(arrLength([5, 0, -4, 1]))
 console.log(arrLength([])) */
 
-function lastElem(arr) {
+/* function lastElem(arr) {
     return arr[arr.length -1]
 }
 
 console.log(lastElem([3, 2, 0, 6, 2]))
 console.log(lastElem(['dog', 'cat', 'ball']))
-console.log(lastElem([null, 5, false]))
+console.log(lastElem([null, 5, false])) */
 
+function arraySum(arr) {
+    return arr.reduce((a, b) => a + b, 0)
+}
+
+console.log(arraySum([2, 2, 2]))
+console.log(arraySum([100, 200, 500]))
+console.log(arraySum([0, -5, -10]))
