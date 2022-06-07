16f3c11852a622283702665b4f50ad32a50db820
gam-c <stavish@gmail.com>
2022-06-07 17:51:44 -0400

    fin

--- app.js
+++ app.js
@@ -72,4 +72,126 @@ console.log(progressiveSum(600)) */
 
 console.log(calcTime(35)) */
 
+/* metodo mio
+function getMax(arr) {
+    return Math.max.apply(null, arr)
+  }
+  
+  console.log(getMax([-100, -200, -300]))  */
 
+/* function getMax(arr) {
+  let largestNumber = arr[0]
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > largestNumber) {
+          largestNumber = arr[i]
+      }
+  }
+  return largestNumber
+}
+
+console.log(getMax([-100, -200, -300]))  */
+
+
+/* metodo mio 
+function reverseString (str) {
+    return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0)
+}
+
+
+console.log(reverseString('abc'))
+console.log(reverseString('Gustavo'))
+console.log(reverseString('This is cool'))
+ */
+
+
+/* function reverseString (str) {
+    let reverseString = ''
+    for (let i = 0; i < str.length; i++) {
+        // this is how to lopp through every chracter in a string, array
+        reverseString = str[i] + reverseString
+    }
+    return reverseString
+}
+ 
+console.log(reverseString('abc'))
+console.log(reverseString('Gustavo'))
+console.log(reverseString('This is cool'))
+ */
+// .split('') string --> array
+// .reverse reverse array
+// .join('') array --> string
+
+/* function convertToZero(arr) {
+    for (let i = 0; i < arr.length; i ++) {
+        arr[i] = 0
+    }
+    return arr
+} */
+
+/* function convertToZero(arr) {
+    return new Array(arr.length).fill(0)
+}
+ */
+/* 
+function convertToZero(arr) {
+    return arr.map(elem => 0)
+}
+
+// .map convierte todo lo que quieras {} need a return
+
+console.log(convertToZero([5, 100, 0]))
+console.log(convertToZero([12]))
+console.log(convertToZero([1, 2, 3, 4, 5])) */
+
+/* function removeApples(arr) {
+    let notApple = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 'Apple') {
+            notApple.push(arr[i])
+            // add element into array .push(arr[i])
+        }
+    } 
+    return notApple
+} */
+
+/* function removeApples(arr) {
+  return arr.filter(elem => elem !== 'Apple')
+}
+// .filter add elem to new arr if condition {} need return
+
+console.log(removeApples(['Banana', 'Apple', 'Orange', 'Apple']))
+console.log(removeApples(['Tomato', 'Orange', 'Banana']))
+console.log(removeApples(['Banana', 'Orange', 'Apple'])) */
+
+/* function filterOutFalsy(arr) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!!arr[i] === true) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+
+
+/* function filterOutFalsy(arr) {
+    return arr.filter(elem => !!elem === true) // !! convert to a boolean
+} */
+
+/* console.log(filterOutFalsy(["", [], 0, null, undefined, "0"]))
+console.log(filterOutFalsy(['Tomato', 'Orange', 'Banana', false]))
+console.log(filterOutFalsy(['Banana', 'Orange', 'Apple']))  */
+
+
+/* function convertToBoolean(arr) {
+    for (let i = 0; i < arr.length; i++) {
+         arr[i] = !!arr[i]
+    }
+    return arr
+} */
+
+/* function convertToBoolean(arr) {
+    return arr.map(elem => !!elem)
+}
+
+console.log(convertToBoolean([500, 0, "David", "", []])) */
